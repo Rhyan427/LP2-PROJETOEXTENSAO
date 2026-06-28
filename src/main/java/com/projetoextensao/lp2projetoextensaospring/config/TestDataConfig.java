b4c66911f59750b3ef9baef8b38a2c8e21a6e25b
Rhyan427 <rhyan.sn@discente.ufma.br>
2026-06-28 14:20:01 -0300

    commit init

--- src/main/java/com/projetoextensao/lp2projetoextensaospring/config/TestDataConfig.java
+++ src/main/java/com/projetoextensao/lp2projetoextensaospring/config/TestDataConfig.java
@@ -5,56 +5,77 @@ import com.projetoextensao.lp2projetoextensaospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
-@Configuration
+@Component
 public class TestDataConfig implements CommandLineRunner {
 
     @Autowired
-    private DiscenteRepo discenteRepo;
-
-    @Autowired
     private CursoRepo cursoRepo;
-
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+    @Autowired
+    private CoordenadorRepo coordRepo;
+    @Autowired
+    private DiscenteRepo discRepo;
+    @Autowired
+    private OportunidadeRepo oportRepo;
     @Autowired
     private PapelRepo papelRepo;
 
     @Override
     public void run(String... args) throws Exception {
-        if (discenteRepo.count() == 0) {
+        // 1. Criar Papéis
+        Papel pDiscente = papelRepo.save(new Papel("DISCENTE"));
+        Papel pCoord = papelRepo.save(new Papel("COORDENADOR"));
 
-            // 1. Criar dados básicos necessários para as FKs (Curso e Papel)
-            Curso curso = new Curso();
-            curso.setNome("Ciência da Computação");
-            curso = cursoRepo.save(curso);
+        // 2. Criar Curso
+        Curso curso = cursoRepo.save(new Curso("Ciência da Computação", 2026, 1000, 500, "Elegivel"));
 
-            Papel papel = new Papel();
-            papel.setDescricao("DISCENTE");
-            papel = papelRepo.save(papel);
+        // 3. Criar 3 Coordenadores
+        for (int i = 1; i <= 3; i++) {
+            Coordenador c = new Coordenador();
+            c.setNome("Prof. Coord " + i);
+            c.setEmail("coord" + i + "@ufma.br");
+            c.setSenha("senha123");
+            c.setSiape("SIAPE" + i);
+            c.setPapel(pCoord);
+            coordRepo.save(c);
+        }
 
-            // 2. Criar Discentes
-            Discente d1 = new Discente();
-            d1.setNome("Ana Silva");
-            d1.setEmail("ana@ufma.br");
-            d1.setSenha("senha123");
-            d1.setAtivo(true);
-            d1.setMatricula("20251001");
-            d1.setCurso(curso);
-            d1.setPapel(papel);
+        // 4. Criar 3 Discentes
+        for (int i = 1; i <= 3; i++) {
+            Discente d = new Discente();
+            d.setNome("Aluno " + i);
+            d.setEmail("aluno" + i + "@ufma.br");
+            d.setSenha("senha123");
+            d.setMatricula("202500" + i);
+            d.setCurso(curso);
+            d.setPapel(pDiscente);
+            discRepo.save(d);
+        }
 
-            Discente d2 = new Discente();
-            d2.setNome("Carlos Eduardo");
-            d2.setEmail("carlos@ufma.br");
-            d2.setSenha("senha456");
-            d2.setAtivo(true);
-            d2.setMatricula("20251002");
-            d2.setCurso(curso);
-            d2.setPapel(papel);
-
-            discenteRepo.saveAll(Arrays.asList(d1, d2));
-
-            System.out.println("Dados de teste carregados com sucesso!");
+        // 5. Criar 3 Oportunidades
+        var coords = coordRepo.findAll();
+        for (int i = 1; i <= 3; i++) {
+            Oportunidade o = new Oportunidade();
+            o.setTitulo("Oportunidade " + i);
+            o.setDescricao("Descrição da Oportunidade " + i);
+            o.setTipo(TipoOportunidade.PROJETO);
+            o.setModalidade(Modalidade.PRESENCIAL);
+            o.setCargaHoraria(20);
+            o.setVagas(10);
+            o.setInicio(LocalDate.now());
+            o.setFim(LocalDate.now().plusMonths(3));
+            o.setStatus(StatusOportunidade.RASCUNHO);
+            o.setResponsavel(coords.get(0)); // Atribui ao primeiro coordenador
+            o.setAutor(coords.get(0));       // Também definido como autor
+            o.setPlano("Plano Padrão");
+            o.setDataPlanoAtividades(LocalDate.now());
+            oportRepo.save(o);
         }
     }
 }
