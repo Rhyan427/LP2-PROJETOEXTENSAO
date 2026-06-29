d3732ccd41f4fd8dc2cdf9153d028d15c8631dab
Rhyan427 <rhyan.sn@discente.ufma.br>
2026-06-28 23:13:10 -0300

    commit init

---
+++ src/main/java/com/projetoextensao/lp2projetoextensaospring/dataTransfer/AvisoData.java
@@ -0,0 +1,28 @@
+package com.projetoextensao.lp2projetoextensaospring.dataTransfer;
+
+import com.projetoextensao.lp2projetoextensaospring.entity.Usuario;
+
+import java.time.LocalDate;
+
+public class AvisoData {
+    private String titulo;
+    private String mensagem;
+    private Usuario autor;
+    private LocalDate dataPublicacao;
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getMensagem() { return mensagem; }
+    public void setMensagem(String mensagem) { this.mensagem = mensagem; }
+    public Usuario getAutor() { return autor; }
+    public void setAutor(Usuario autor) { this.autor = autor; }
+    public LocalDate getDataPublicacao() { return dataPublicacao; }
+
+    public AvisoData(String titulo, String mensagem, Usuario autor, LocalDate dataPublicacao) {
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+        this.autor = autor;
+        this.dataPublicacao = LocalDate.now();
+    }
+
+}
